cd7fc1fe81c9bc49e52c03882b4d0f4cc422e42f
Vatan Aksoy Tezer <vatan@picknik.ai>
2022-08-18 22:06:25 +0000

    More code review and cleaning

--- c3pzero_ignition/src/odom2tf_broadcaster_node.cpp
+++ c3pzero_ignition/src/odom2tf_broadcaster_node.cpp
@@ -11,36 +11,31 @@ using std::placeholders::_1;
  * @brief Simple class that broadcasts tf from the odometry message
  *
  */
-class Odom2TfBroadcaster : public rclcpp::Node
+struct Odom2TfBroadcaster : public rclcpp::Node
 {
-public:
-  Odom2TfBroadcaster() : Node("odom2tf_broadcaster")
-  {
-    subscription_ = this->create_subscription<nav_msgs::msg::Odometry>(
-        "odom", 10, std::bind(&Odom2TfBroadcaster::topic_callback, this, _1));
-    tf_broadcaster_ = std::make_shared<tf2_ros::TransformBroadcaster>(shared_from_this());
-  }
-
+  Odom2TfBroadcaster()
+  : Node("odom2tf_broadcaster"), 
+    tf_broadcaster_{std::make_shared<tf2_ros::TransformBroadcaster>(shared_from_this())},
+    subscription_{this->create_subscription<nav_msgs::msg::Odometry>(
+        "odom", 10, [this](nav_msgs::msg::Odometry::ConstSharedPtr msg){
+          geometry_msgs::msg::TransformStamped transform_stamped;
+          transform_stamped.header = msg->header;
+          transform_stamped.child_frame_id = msg->child_frame_id;
+          transform_stamped.transform.translation.x = msg->pose.pose.position.x;
+          transform_stamped.transform.translation.y = msg->pose.pose.position.y;
+          transform_stamped.transform.translation.z = msg->pose.pose.position.z;
+          transform_stamped.transform.rotation.x = msg->pose.pose.orientation.x;
+          transform_stamped.transform.rotation.y = msg->pose.pose.orientation.y;
+          transform_stamped.transform.rotation.z = msg->pose.pose.orientation.z;
+          transform_stamped.transform.rotation.w = msg->pose.pose.orientation.w;
+          tf_broadcaster_->sendTransform(transform_stamped);
+        })}
+  {}
 private:
-  void topic_callback(const nav_msgs::msg::Odometry::SharedPtr msg)
-  {
-    transform_stamped_.header = msg->header;
-    transform_stamped_.child_frame_id = msg->child_frame_id;
-    transform_stamped_.transform.translation.x = msg->pose.pose.position.x;
-    transform_stamped_.transform.translation.y = msg->pose.pose.position.y;
-    transform_stamped_.transform.translation.z = msg->pose.pose.position.z;
-    transform_stamped_.transform.rotation.x = msg->pose.pose.orientation.x;
-    transform_stamped_.transform.rotation.y = msg->pose.pose.orientation.y;
-    transform_stamped_.transform.rotation.z = msg->pose.pose.orientation.z;
-    transform_stamped_.transform.rotation.w = msg->pose.pose.orientation.w;
-
-    tf_broadcaster_->sendTransform(transform_stamped_);
-  }
-  rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr subscription_;
   std::shared_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster_;
-  geometry_msgs::msg::TransformStamped transform_stamped_;
-
+  rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr subscription_;
 };
+
 
 int main(int argc, char* argv[])
 {
