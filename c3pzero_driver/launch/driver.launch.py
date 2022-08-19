a041fc019e1a4d0160f5aff202dc2c0ee8fc9084
Griswald Brooks <griswald.brooks@picknik.ai>
2022-08-19 21:37:54 +0000

    Lint, add deps

--- c3pzero_driver/launch/driver.launch.py
+++ c3pzero_driver/launch/driver.launch.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import os
 
 from ament_index_python.packages import get_package_share_directory
@@ -7,9 +8,12 @@ import launch_ros.actions
 
 
 def generate_launch_description():
-    return launch.LaunchDescription([
-        launch_ros.actions.Node(
-            package='c3pzero_driver', executable='twist2roboclaw',
-            name='driver',
+    return launch.LaunchDescription(
+        [
+            launch_ros.actions.Node(
+                package="c3pzero_driver",
+                executable="twist2roboclaw",
+                name="driver",
             ),
-    ])
+        ]
+    )
